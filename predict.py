fb1512050f06212681ada108c86d1449ed0436f1
Shadi Ghantous <54699679+Shadiouss@users.noreply.github.com>
2024-07-26 10:19:29 +0200

    Update predict.py

--- predict.py
+++ predict.py
@@ -16,35 +16,44 @@ import pandas as pd
 
 
 # save the predicted tiles
-def store_tif(output_folder, output_array, dtype, geo_transform, geo_proj, nodata_value):
+def store_tif(output_folder, output_array, dtype, geo_transform, geo_proj, nodata_value, class_zero= False):
     """Stores a tif file in a specified folder."""
     driver = gdal.GetDriverByName('GTiff')
 
     if len(output_array.shape) == 3:
         out_ds = driver.Create(str(output_folder), output_array.shape[2], output_array.shape[1], output_array.shape[0],
-                                dtype)
+                               dtype)
     else:
         out_ds = driver.Create(str(output_folder), output_array.shape[1], output_array.shape[0], 1, dtype)
     out_ds.SetGeoTransform(geo_transform)
 
     out_ds.SetProjection(geo_proj)
+    
+    if class_zero:
+        # Process the output array to handle class definitions
+        processed_array = np.where(output_array == 0, nodata_value, output_array - 1)  # Class 0 as NaN and decrement other classes by 1
+    else:
+        processed_array = output_array
+
+
     if len(output_array.shape) == 3:
         for b in range(output_array.shape[0]):
-            out_ds.GetRasterBand(b + 1).WriteArray(output_array[b])
+            out_ds.GetRasterBand(b + 1).WriteArray(processed_array[b])
     else:
-        out_ds.GetRasterBand(1).WriteArray(output_array)
+        out_ds.GetRasterBand(1).WriteArray(processed_array)
 
-    # loop through the image bands to set nodata
+    # Loop through the image bands to set nodata
     if nodata_value is not None:
         for i in range(1, out_ds.RasterCount + 1):
-            # set the nodata value of the band
+            # Set the nodata value of the band
             out_ds.GetRasterBand(i).SetNoDataValue(nodata_value)
 
     out_ds.FlushCache()
     out_ds = None
-    
-# create valid figures 
-def plot_valid_predict(output_folder, truth_label, regression=False, merge=False):
+
+
+# create valid figures
+def plot_valid_predict(output_folder, predict_path, regression=False, merge=False, class_zero= False):
     assert not (merge and regression), "Both merge and regression cannot be True"
 
     if merge:
@@ -55,38 +64,42 @@ def plot_valid_predict(output_folder, truth_label, regression=False, merge=False
     # Create a new folder to save the figures
     valid_path = os.path.join(output_folder, "Valid_figures")
     os.makedirs(valid_path, exist_ok=True)
-    
+
     # Replace the last part of the truth_label path
-    truth_label = truth_label.replace('img_tiles', 'mask_tiles')
+    truth_label = predict_path.replace('img_tiles', 'mask_tiles')
 
     y_true = []
     y_pred = []
-    
+
     for file_name in os.listdir(output_folder):
         if file_name.endswith('.tif'):
             pred_path = os.path.join(output_folder, file_name)
             true_path = os.path.join(truth_label, file_name)
-            
+
             with rasterio.open(pred_path) as src_pred:
-                pred_data = src_pred.read(1)  # Assuming single band for class labels
-                
+                pred_data = src_pred.read(1).astype(np.int64)  # Assuming single band for class labels
+
             with rasterio.open(true_path) as src_true:
-                true_data = src_true.read(1)  # Assuming single band for class labels
-            
+                true_data = src_true.read(1).astype(np.int64)  # Assuming single band for class labels
+
             # Determine the most frequent class in the tile
             pred_class = np.argmax(np.bincount(pred_data.flatten()))
             true_class = np.argmax(np.bincount(true_data.flatten()))
-            
+
+            if class_zero:
+                true_class = true_class[true_class != 0] - 1
+
+
             y_true.append(true_class)
             y_pred.append(pred_class)
-    
+
     if not y_true or not y_pred:
         raise ValueError("No valid tiles found for evaluation")
-    
+
     # Compute the confusion matrix
     cm = confusion_matrix(y_true, y_pred)
     class_report = classification_report(y_true, y_pred, zero_division=1)
-    
+
     # Plot the classification report and get class names
     report_data = []
     class_names = []
@@ -101,19 +114,19 @@ def plot_valid_predict(output_folder, truth_label, regression=False, merge=False
             'precision': float(row_data[1]),
             'recall': float(row_data[2]),
             'f1_score': float(row_data[3]),
-            'support': int(row_data[4])
+            'support': int(float(row_data[4]))
         }
         report_data.append(row)
-    
+
     dataframe = pd.DataFrame.from_dict(report_data)
-    
+
     plt.figure(figsize=(10, 7))
     sns.heatmap(dataframe.set_index('class'), annot=True, fmt='.2f', cmap='crest')
     plt.title('Classification Report')
     classification_report_path = os.path.join(valid_path, "classification_report.png")
     plt.savefig(classification_report_path)
     plt.show()
-    
+
     # Plot the confusion matrix with class names
     plt.figure(figsize=(10, 7))
     sns.heatmap(cm, annot=True, fmt='d', cmap='crest', xticklabels=class_names, yticklabels=class_names)
@@ -123,7 +136,7 @@ def plot_valid_predict(output_folder, truth_label, regression=False, merge=False
     confusion_matrix_path = os.path.join(valid_path, "Confusion_Matrix.png")
     plt.savefig(confusion_matrix_path)
     plt.show()
-    
+
     print("Confusion Matrix:")
     print(cm)
     print("\nClassification Report:")
@@ -133,7 +146,7 @@ def plot_valid_predict(output_folder, truth_label, regression=False, merge=False
 
 
 def save_predictions(predict_model, predict_path, regression, merge=False, all_classes=False, specific_class=None,
-                     large_file=False, AOI=None, year=None, validation_vision = True):
+                     large_file=False, AOI=None, year=None, validation_vision=True, class_zero=False):
     """
     Runs a prediction on all tiles within a folder and stores predictions in the predict_tiles folder
 
@@ -151,11 +164,14 @@ def save_predictions(predict_model, predict_path, regression, merge=False, all_c
     path = Path(predict_path)
 
     # Define the path as the current directory
+    # current_directory = Path(os.getcwd())
+    # Define the 'models' directory
+    # output_folder_ = current_directory / 'Prediction'
     if not merge:
         output_folder = path.parent / ('predicted_tiles_' + Path(predict_model).stem)
     else:
-        output_folder = path.parent 
-        
+        output_folder = path.parent
+
     model_name = os.path.basename(predict_model).split('.')[0]
 
     if not os.path.exists(output_folder):
@@ -172,12 +188,12 @@ def save_predictions(predict_model, predict_path, regression, merge=False, all_c
     t = time.localtime()
     current_time = time.strftime("%H:%M:%S", t)
     print(f'Started at: {current_time}')
-    #for i in range(len(tiles)):
+    # for i in range(len(tiles)):
     for i in tqdm(range(len(tiles)), desc='Processing tiles'):
-        #print(f'Current progress: {i}/{len(tiles)}')
+        # print(f'Current progress: {i}/{len(tiles)}')
         tile_preds = learn.predict(Path(tiles[i]), with_input=False)
-        # if single class creates issues try: class_lst = tile_preds[1]
         class_lst = []
+
         if regression:
             for cl in range(len(tile_preds[1])):
                 class_lst.append(tile_preds[1][cl])
@@ -232,12 +248,13 @@ def save_predictions(predict_model, predict_path, regression, merge=False, all_c
                 class_lst *= ((128 / 4) - 1)
                 class_lst = np.around(class_lst).astype(np.int8)
                 dtype = gdal.GDT_Byte
-                store_tif(str(output_folder) + "\\" + os.path.basename(tiles[i]), class_lst, dtype, geotrans,geoproj, None)
+                store_tif(str(output_folder) + "\\" + os.path.basename(tiles[i]), class_lst, dtype, geotrans, geoproj,
+                          None)
             else:
-                store_tif(str(output_folder) + "\\" + os.path.basename(tiles[i]), class_lst.numpy(), dtype, geotrans, geoproj, None)
-    # plot the confusion matric and classification report
+                store_tif(str(output_folder) + "\\" + os.path.basename(tiles[i]), class_lst.numpy(), dtype, geotrans,
+                          geoproj, None)
     if validation_vision:
-        plot_valid_predict(output_folder, predict_path, regression, merge)
+        plot_valid_predict(output_folder, predict_path, regression, merge, class_zero)
     if merge:
         # go through the information for all tiles, find upper left most corner and lower right most corner
         # --> these define the extend of the final output
@@ -295,9 +312,9 @@ def save_predictions(predict_model, predict_path, regression, merge=False, all_c
             # divide raster by counter to turn overlaps into realistic values
             merged_raster[merge_counter > 0] /= merge_counter[merge_counter > 0]
 
-            #set raster to -9999 where no predictions were placed
+            # set raster to -9999 where no predictions were placed
             nodata = -9999
-            merged_raster[merge_counter == 0] = nodata #maybe change to merged_raster[merged_raster == 0] = nodata
+            merged_raster[merge_counter == 0] = nodata  # maybe change to merged_raster[merged_raster == 0] = nodata
         else:
             if large_file:
 
@@ -314,7 +331,7 @@ def save_predictions(predict_model, predict_path, regression, merge=False, all_c
                 pass
             elif specific_class is None:
                 # for decoded argmax value
-                #merged_raster = np.where(merged_raster.max(axis=0) < 0.1, 14, merged_raster.argmax(axis=0)) #check if this is still relevant?
+                # merged_raster = np.where(merged_raster.max(axis=0) < 0.1, 14, merged_raster.argmax(axis=0)) #check if this is still relevant?
                 merged_raster = merged_raster.argmax(axis=0)
             else:
                 # for probabilities of specific class [1] -> klasse 1
@@ -328,17 +345,14 @@ def save_predictions(predict_model, predict_path, regression, merge=False, all_c
         else:
             dtype = gdal.GDT_Byte
 
-
         # Define the parameters for the name of output:
         output_file_name_parts = [AOI, year, model_name, "prediction"]
         output_file_name = "_".join(filter(None, output_file_name_parts)) + ".tif"
         output_file = os.path.join(output_folder, output_file_name)
         print(output_file)
-        
-        store_tif(output_file , merged_raster, dtype,
+
+        store_tif(output_file, merged_raster, dtype,
                   [upleft_x_full, geotrans_for_merge[0, 2], 0.0, upleft_y_full, 0.0, geotrans_for_merge[0, 5]],
                   geoproj_for_merge, nodata)
-
-        
 
         print(f"Prediction stored in {output_folder}.")
