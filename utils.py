4422823ade2483cf985a9e4e3c7c5f756dcb9039
Shadi Ghantous <54699679+Shadiouss@users.noreply.github.com>
2024-10-24 14:13:30 +0200

    Update utils.py

--- utils.py
+++ utils.py
@@ -202,9 +202,7 @@ class SegmentationAlbumentationsTransform(ItemTransform):
         This transform expects input data in the form of tuples (image, mask).
         If only images are provided, it assumes no masks are present.
     """
-    split_idx = 0  # Apply Augmentations for 0 = Train, 1 = Validation, None = Both
-
-    def __init__(self, dtype, aug, n_transform_imgs=2, **kwargs):
+    def __init__(self, dtype, aug, n_transform_imgs=2, split_idx= 0, **kwargs):
         """
         Initializes the SegmentationAlbumentationsTransform.
 
@@ -216,6 +214,7 @@ class SegmentationAlbumentationsTransform(ItemTransform):
         self.aug = aug
         self.n_transform_imgs = n_transform_imgs
         self.dtype = dtype
+        self.split_idx = split_idx
 
     def encodes(self, x):
         """
@@ -253,9 +252,8 @@ class SegmentationAlbumentationsTransform(ItemTransform):
 
         # Process each image and mask in the last proportion of the batch
         else:
-            for img, mask in zip(batch_img[int(n_transform - len(batch_img)):],
-                                 batch_mask[int(n_transform- len(batch_img)):]):
-
+            for img, mask in zip(batch_img[:int(n_transform - len(batch_img))],
+                                 batch_mask[:int(n_transform - len(batch_img))]):
                 # Permute the image dimensions from (C, H, W) to (H, W, C) for albumentations
                 img = img.permute(1, 2, 0)  # Now shape is [W, H, C]
 
@@ -285,15 +283,16 @@ class SegmentationAlbumentationsTransform(ItemTransform):
 
         # Leave the first proportion of the batch unchanged
 
-        for img, mask in zip(batch_img[:int(n_transform - len(batch_img))],
-                             batch_mask[:int(n_transform - len(batch_img))]):
+        for img, mask in zip(batch_img[int(n_transform - len(batch_img)):],
+                             batch_mask[int(n_transform- len(batch_img)):]):
+            if self.dtype == 'int16':
+                img /= 255
+            
             # Append the unchanged images and masks to the transformed lists
             transformed_images.append(img)
             transformed_masks.append(mask)
         # Stack all processed items in the batch back into tensors
         return torch.stack(transformed_images), torch.stack(transformed_masks)
-
-
 
 def save_params(params, model_Path, description):
     """
